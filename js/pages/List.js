530b50ae6c38ea3080fa7279bf8ee05df5050dad
GDNoob402 <gmd.noo8@gmail.com>
2026-02-21 15:30:04 +1100

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -71,7 +71,7 @@ export default {
                                 <img v-if="record.mobile" :src="\`/assets/phone-landscape\${store.dark ? '-dark' : ''}.svg\`" alt="Mobile">
                             </td>
                             <td class="hz">
-                                <p>{{ record.hz }}Enjoyme</p>
+                                <p>{{ record.hz }} Enjoyme</p>
                             </td>
                         </tr>
                     </table>
