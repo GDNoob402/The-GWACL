7c071111646b4496ca7239b8dccb14cc04fc62a9
GDNoob402 <gmd.noo8@gmail.com>
2026-02-21 13:23:39 +1100

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -124,7 +124,7 @@ export default {
                         Level CAN NOT be over 14cps
                     </p>
                     <p>
-                        Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level
+                        Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off then afterwards we never accept records for said level
                     </p>
                 </div>
             </div>
